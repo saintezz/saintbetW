4b881b33cc12988320023e90d4550fd4ca9fd689
saintezz <andsyygg@gmail.com>
2026-09-09 20:30:40 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const fs = require("fs");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
+app.options("*", cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(__dirname, { index: false }));
 
